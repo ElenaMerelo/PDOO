edfe46b1c248822bd9287d38da30411af0e34bf8
elenamerelo <nenamer@gmail.com>
2018-02-28 16:40:03 +0100

    Primer programa hecho en java wohoo

---
+++ Hello_world.java
@@ -0,0 +1,13 @@
+/*
+* @brief Simple program that prints hello world
+* @author Elena Merelo
+*/
+package hello_world;
+
+public class Hello_world {
+
+    public static void main(String[] args) {
+        System.out.println("Hey there! I am using Java!");
+    }
+    
+}
