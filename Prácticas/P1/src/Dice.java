04d912bcc7c5c707f821730686b6d041ec69cd01
elenamerelo <nenamer@gmail.com>
2018-03-01 13:29:50 +0100

    Ya casi terminando

--- Prácticas/P1/src/Dice.java
+++ Prácticas/P1/src/Dice.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+
 /**
  * @author elena
  * @brief Esta clase tiene la responsabilidad de tomar todas las decisiones 
@@ -11,10 +14,55 @@ class Dice {
     
     //Constructores
     Dice(){
+        generator= new Random();
         NHANGARSPROB=0.25f;
         NSHIELDSPROB=0.25f;
         NWEAPONSPROB=0.33f;
         FIRSTSHOTPROB=0.5f;
     }
+    
+    /*
+     * @brief Devuelve 0 con una probabilidad de NHANGARSPROB y 1 en caso contrario. 
+     * Este método determina el número de hangares que recibirá una estación espacial al ser creada.
+    */
+    int initWithNHangars(){
+        if( generator.nextFloat() <= NHANGARSPROB )
+            return 0;
+        return 1;
+    }
+    
+    /*
+     * @brief Devuelve 1 con una probabilidad de NWEAPONSPROB, 2 con la misma probabilidad 
+     * y 3 con una probabilidad de (1-2* NWEAPONSPROB). Este método determina el
+     * número de armas que recibirá una estación espacial al ser creada.
+    */
+    int initWithWeapons(){
+        float n= 1 - 2*NWEAPONSPROB;
+        if ( generator.nextFloat() < NWEAPONSPROB )
+            return 1;
+        if (generator.nextFloat() >= NWEAPONSPROB && generator.nextFloat() < n )
+            return 2;
+        return 3; 
+    }
+    
+    /*
+     * @brief Devuelve 0 con una probabilidad de NSHIELDSPROB y 1 en caso contrario.
+     * Este método determina el número de potenciadores de escudo que recibirá 
+     * una estación espacial al ser creada.
+    */
+    int initWithNShields(){
+        return generator.nextFloat() <= NSHIELDSPROB ? 0: 1;
+    }
+    
+    /*
+     * @brief Genera un número aleatorio del intervalo [0,nPlayers-1]. Determina 
+     * el jugador (su índice) que iniciará la partida.
+    */
+    int whoStarts(int nPlayers){
+        return generator.ints(0, nPlayers-1).findFirst().getAsInt();
+    }
+    
+    
+    
             
 }
