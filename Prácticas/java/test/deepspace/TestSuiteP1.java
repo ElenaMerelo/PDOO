db4b0b60f5acee9aace709669e87f050586e074f
elenamerelo <nenamer@gmail.com>
2018-03-25 09:57:05 +0200

    Netbeans es una caca. Necesito organizar carpetas, no encuentra carpeta de tests aunque exista :(

---
+++ Prácticas/java/test/deepspace/TestSuiteP1.java
@@ -0,0 +1,16 @@
+/**
+ * @author Elena Merelo
+ */
+
+// package deepspace.test.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({WeaponTypeTest.class})
+
+public class TestSuiteP1 {
+    
+}
