e7436d3308a033c06f3aa444e0fb551b4c1b0bbe
elenamerelo <nenamer@gmail.com>
2018-05-10 22:14:55 +0200

    SpaceCity done

--- Prácticas/ruby/lib/SpaceCity.rb
+++ Prácticas/ruby/lib/SpaceCity.rb
@@ -1,9 +1,39 @@
 require_relative 'SpaceStation'
 
 module Deepspace
+  attr_reader :collaborators
+  
   class SpaceCity < SpaceStation
-    def initialize
-      
+    def initialize(base, rest)
+      super(base)
+      @base= base 
+      @collaborators= rest
     end
+    
+    def fire
+      f= base.fire 
+      
+      for s in @collaborators
+        f += s.fire
+      end
+      
+      f
+    end
+    
+    def protection
+      p= base.protection 
+      
+      for s in @collaborators
+        p += s.protection 
+      end
+      
+      p
+    end
+    
+    def setLoot(l)
+      super.setLoot(l) 
+      Transformation::NOTRANSFORM
+    end
+    
   end
 end
