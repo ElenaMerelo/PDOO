da2a2570c9990904b6dc08b7c3ecc500760f9d7f
elenamerelo <nenamer@gmail.com>
2018-06-24 17:11:53 +0200

    PowerEfficientSpaceStation ok

--- Prácticas/ruby/lib/PowerEfficientSpaceStation.rb
+++ Prácticas/ruby/lib/PowerEfficientSpaceStation.rb
@@ -1,25 +1,31 @@
 require_relative 'SpaceStation'
-
+require_relative 'Transformation'
+require_relative 'PowerEfficientSpaceStationToUI'
 
 module Deepspace
   class PowerEfficientSpaceStation < SpaceStation
     @@EFFICIENCYFACTOR= 1.1.freeze
     
     def initialize(station)
-      newCopy(station)
+      # super.newCopy(st)  o bien:  super(st.name,st.getSupplies, st.nMedals, st.weapons,st.shieldBoosters,st.hangar,st.pendingDamage)
+      s = SuppliesPackage.new(station.ammoPower, station.fuelUnits, station.shieldPower)
+     super(station.name, s, station.weapons, station.shieldBoosters, station.nMedals)
     end
     
     def fire
-      super.fire * @@EFFICIENCYFACTOR
+      super * @@EFFICIENCYFACTOR
     end
     
     def protection
-      super.protection *@@EFFICIENCYFACTOR
+      super * @@EFFICIENCYFACTOR
     end
     
     def setLoot(l)
-      super
-      Transformation::NOTRANSFORM
+      if super==Transformation::GETEFFICIENT
+        return Transformation::GETEFFICIENT
+      else
+        return Transformation::NOTRANSFORM
+      end
     end
     
     def getUIversion
